604746719a1525f0652b8eb51b8051e097cb3189
blahbaka <penelope.zhong@proton.me>
2025-06-10 21:13:16 -0500

    init
    auto-fomrat
    
    cool stuffs
    
    stuffs
    
    asr
    
    format
    
    fold stuff
    
    wow
    
    init
    
    auto-fomrat
    
    cool stuffs
    
    stuffs
    
    asr
    
    format
    
    fold stuff
    
    wow
    
    minor changes
    
    move to pack

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -1,13 +1,14 @@
-return {
-  {
-    'nvim-tree/nvim-tree.lua',
-    lazy = false,
-    dependencies = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    },
-    config = function() require('nvim-tree').setup {} end,
-    keys = {
-      { '\\', ':NvimTreeFindFileToggle<CR>', desc = 'Toggle nvim-tree', silent = true },
-    },
-  },
+
+local plugins = {
+  'https://github.com/nvim-tree/nvim-tree.lua',
 }
+
+if vim.g.have_nerd_font then
+  table.insert(plugins, 'https://github.com/nvim-tree/nvim-web-devicons') -- not strictly required, but recommended
+end
+
+vim.pack.add(plugins)
+
+require('nvim-tree').setup {}
+
+vim.keymap.set('n', '\\', '<Cmd>NvimTreeFindFileToggle<CR>', { desc = 'Toggle nvim-tree', silent = true })
